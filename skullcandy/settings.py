f0ccb0e552a2038d3cc2f606119db54bbb52aa8a
krithikGR <krithik.23pmc135@mariancollege.org>
2024-10-02 14:20:46 +0530

    first commit

--- skullcandy/settings.py
+++ skullcandy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0j#f6y20)3&%jg-9xrdv7w7nf4f$^82o0yb%x&4qq&$km6wgur
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
